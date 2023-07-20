f19022e35bb25b703b2ce7f347dcd96021896203
Fruitellar <109595648+Fruitellar@users.noreply.github.com>
2023-07-20 19:45:22 +0800

    changed to singleton pattern and added dimensions for widgets upon loading and saving

--- src/services/layout/layout_manager.py
+++ src/services/layout/layout_manager.py
@@ -1,23 +1,58 @@
 from typing import Any, Generic, TypeVar
 
 from utils.configuration_parser import ConfigurationParser
-from widgets.table_layout.table_template import MainTableTemplate, TableTemplate
+from widgets.table_layout.table_template import TableTemplate
 
 T = TypeVar("T", bound=TableTemplate)
 
 
 class TableLayOutManager(Generic[T]):
+    _shared_instance = None
+
+    def __new__(cls, *args, **kwargs):
+        if not cls._shared_instance:
+            cls._shared_instance = super(TableLayOutManager, cls).__new__(cls)
+        return cls._shared_instance
+
     def __init__(self, configuration_parser: type[ConfigurationParser]):
-        self.table_templates: list[T] = []
-        self.configuration_parser = configuration_parser
-        self.table_settings: dict[Any, Any] = dict()
-        self.layout_settings: dict[Any, Any] = dict()
+        if not hasattr(self, "initialized"):
+            self.table_templates: list[T] = []
+            self.configuration_parser = configuration_parser
+            self.initialized = True
 
-        self.load_settings()
+    @classmethod
+    def get_instance(cls, configuration_parser: type[ConfigurationParser]):
+        if not cls._shared_instance:
+            cls._shared_instance = TableLayOutManager(configuration_parser)
+        return cls._shared_instance
 
-    def load_settings(self):
-        self.table_settings = self.configuration_parser.read_table_configuration()
-        self.layout_settings = self.configuration_parser.read_layout_configuration()
+    @property
+    def table_settings(self) -> dict:
+        return self.configuration_parser.read_table_configuration()
+
+    @property
+    def layout_settings(self) -> dict:
+        return self.configuration_parser.read_layout_configuration()
+
+    @property
+    def table_configurations(self) -> dict:
+        return self.configuration_parser.read_layout_configuration()[
+            "table_configurations"
+        ]
+
+    def save_table_configuration(self):
+        table_configurations = dict()
+        for index, table in enumerate(self.table_templates):
+            table_configurations["slot_" + str(index + 1)] = {
+                "top": table.geometry().top(),
+                "left": table.geometry().left(),
+            }
+        self.configuration_parser.write_layout_configuration(
+            table_configurations=table_configurations
+        )
+
+    def save_table_count(self, table_count: int):
+        self.configuration_parser.write_layout_configuration(table_count=table_count)
 
     def show_templates(
         self, cls_table_template: type[T], cls_main_table_template: type[T]
@@ -25,28 +60,34 @@ class TableLayOutManager(Generic[T]):
         """
         table_template is a widget
         """
-        self.load_settings()  # reload settings to check if any changes have been made
         self.table_templates = []  # destroy any widget so not to repopulate it again
 
         main_table_template = cls_main_table_template(
-            left=0,
-            top=0,
+            left=self.table_configurations["slot_1"]["left"],
+            top=self.table_configurations["slot_1"]["top"],
             table_height=self.table_settings.get("table_height", 0),
             table_width=self.table_settings.get("table_width", 0),
             number_label=1,
         )
         self.table_templates.append(main_table_template)
-        for number_label in range(2, self.layout_settings.get("table_count", 1) + 2):
+        for number_label in range(2, self.layout_settings.get("table_count", 1) + 1):
+            left = self.table_configurations.get("slot_" + str(number_label)).get(
+                "left", 0
+            )
+            top = self.table_configurations.get("slot_" + str(number_label)).get(
+                "top", 0
+            )
             table_template = cls_table_template(
-                left=0,
-                top=0,
+                left=left,
+                top=top,
                 table_height=self.table_settings.get("table_height", 0),
                 table_width=self.table_settings.get("table_width", 0),
                 number_label=number_label,
             )
             self.table_templates.append(table_template)
-        self.table_templates.reverse()  # slot 1 should appear last and should be visible right away
-        [table_template.show() for table_template in self.table_templates]
+        [table_template.show() for table_template in reversed(self.table_templates)]
 
 
-table_layout_manager: TableLayOutManager = TableLayOutManager(ConfigurationParser)
+table_layout_manager = TableLayOutManager.get_instance(
+    configuration_parser=ConfigurationParser
+)
