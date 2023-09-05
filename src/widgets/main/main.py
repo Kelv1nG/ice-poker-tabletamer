dabbf94aed73f54f6a042095190637ddaaedbfe9
Fruitellar <109595648+Fruitellar@users.noreply.github.com>
2023-09-05 09:01:18 +0800

    renamed file names

--- src/widgets/main/main.py
+++ src/widgets/main/main.py
@@ -1,7 +1,7 @@
 from PyQt6 import uic
 from PyQt6.QtWidgets import QMainWindow
 
-from . import layout_setup, main_setup, table_setup
+from . import layout_setup_methods, main_methods, table_setup_methods
 
 
 class MainWindow(QMainWindow):
@@ -12,9 +12,9 @@ class MainWindow(QMainWindow):
         self.setWindowTitle("CurbYourTables")
 
         # init methods
-        table_setup.load_settings(self.ui)
-        layout_setup.load_settings(self.ui)
-        main_setup.hide_labels_on_stop(self.ui)
+        table_setup_methods.load_settings(self.ui)
+        layout_setup_methods.load_settings(self.ui)
+        main_methods.hide_labels_on_stop(self.ui)
 
         self._worker = None
         self._thread = None
@@ -25,26 +25,26 @@ class MainWindow(QMainWindow):
     def connect_buttons(self):
         def setup_table():
             self.ui.table_setup_search.clicked.connect(
-                lambda: table_setup.search_table(self.ui)
+                lambda: table_setup_methods.search_table(self.ui)
             )
             self.ui.table_setup_save.clicked.connect(
-                lambda: table_setup.save_settings(self.ui)
+                lambda: table_setup_methods.save_settings(self.ui)
             )
 
         def visualize_grid():
-            self.ui.visualize_grid.clicked.connect(layout_setup.show_layout)
+            self.ui.visualize_grid.clicked.connect(layout_setup_methods.show_layout)
 
         def add_reduce_tables():
             self.ui.add_table_count.clicked.connect(
-                lambda: layout_setup.add_table_count(self.ui)
+                lambda: layout_setup_methods.add_table_count(self.ui)
             )
             self.ui.reduce_table_count.clicked.connect(
-                lambda: layout_setup.reduce_table_count(self.ui)
+                lambda: layout_setup_methods.reduce_table_count(self.ui)
             )
 
         def connect_application_start():
             self.ui.start_button.clicked.connect(
-                lambda: main_setup.on_start(self, self.ui)
+                lambda: main_methods.on_start(self, self.ui)
             )
 
         setup_table()
