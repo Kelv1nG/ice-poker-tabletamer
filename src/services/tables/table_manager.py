0ce36e53a3829328c010cb6ec1ba8e8d79e7b203
Fruitellar <109595648+Fruitellar@users.noreply.github.com>
2023-09-09 09:05:29 +0800

    refactored and added table swapping functionality when assigning to a slot

--- src/services/tables/table_manager.py
+++ src/services/tables/table_manager.py
@@ -1,7 +1,7 @@
-import math
-from dataclasses import dataclass
 
+import math
 import pygetwindow as gw
+
 
 from services.layout.layout_manager import TableLayOutManager
 
@@ -10,6 +10,12 @@ from .entities import Slot
 from .table_config import TableConfiguration
 from .utilities import AppName, WindowsSelector
 from .events import EventType
+from services.input_controllers.mouse_listener import mouse_listener
+from services.input_controllers.entities import key_states
+
+# start the mouse listener
+# this function will start listening to mouse events
+mouse_listener.start()
 
 class SlotManager:
     """
@@ -19,7 +25,7 @@ class SlotManager:
     def __init__(self):
         self._slots: dict[str, Slot] = {}
 
-    def allocate_window_to_slot(self, slot_num: str, process_window: gw.Window) -> None:
+    def allocate_window_to_slot(self, slot_num: str, window: gw.Window) -> None:
         """
         allocate a window to a specified slot.
 
@@ -33,7 +39,7 @@ class SlotManager:
         """
         if slot_num in self._slots:
             if self._slots[slot_num].window is None:
-                self._slots[slot_num].window = process_window
+                self._slots[slot_num].window = window
                 self._slots[slot_num].resize_window()
                 self._slots[slot_num].move_to_assigned_slot()
             else:
@@ -41,7 +47,7 @@ class SlotManager:
         else:
             raise exceptions.InvalidSlotNum(slot_num)
 
-    def deallocate_slot(self, slot_num: str) -> None:
+    def deallocate_window_from_slot(self, slot_num: str) -> gw.Window:
         """
         deallocate a window from a specified slot.
 
@@ -55,7 +61,8 @@ class SlotManager:
         if slot_num in self._slots:
             slot = self._slots[slot_num]
             if slot.window:
-                slot.window = None
+                window, slot.window = slot.window, None
+                return window
             else:
                 raise exceptions.EmptySlot(slot_num)
         else:
@@ -70,7 +77,7 @@ class SlotManager:
         if len(empty_slots) > 0:
             # assign to first empty slot in order
             slot_num = next(iter(empty_slots.keys()))
-            self.allocate_window_to_slot(slot_num=slot_num, process_window=window)
+            self.allocate_window_to_slot(slot_num=slot_num, window=window)
 
     def remove_window_from_slot(self, window: gw.Window) -> None:
         """
@@ -79,7 +86,58 @@ class SlotManager:
         """
         for slot_num, slot in self.slots.items():
             if slot.window == window:
-                self.deallocate_slot(slot_num)
+                self.deallocate_window_from_slot(slot_num)
+
+    def get_closest_slot_to_window(self, window: gw.Window, assign_to_empty_slot: bool = False) -> str:
+        """
+        gets the closest slot num given a window
+
+        args:
+            window: window in which distance will be calculated
+            assign_to_empty_slot (bool, optional): Whether to assign to an empty slot. Defaults to False.
+        returns:
+            str: closest slot num
+        """
+        window_center = (window.centery, window.centerx)
+
+        # Filter the slots based on the assign_to_empty_slot flag
+        if assign_to_empty_slot:
+            filtered_slots = {slot_num: slot for slot_num, slot in self._slots.items() if slot.window is None}
+        else:
+            filtered_slots = self._slots
+
+        # Find the closest slot
+        closest_slot_num = min(filtered_slots, key=lambda slot_num: self.calculate_distance(
+            win_coord=window_center, slot_coord=filtered_slots[slot_num].slot_center_coordinates))
+
+        return closest_slot_num
+
+    def assign_window_to_closest_slot(self, window: gw.Window):
+        """
+        Assigns a window to the closest slot.
+
+        If the closest slot is occupied, it swaps the windows between the slots.
+        else just moves it to the closest slot
+        """
+        closest_slot_num = self.get_closest_slot_to_window(window)
+        orig_slot_num = self.get_slot_num_from_window(window)
+
+        closest_slot = self._slots[closest_slot_num]
+
+        if closest_slot.window is not None:
+            # swap the windows between the slots
+            deallocated_window = self.deallocate_window_from_slot(closest_slot_num)
+            self.allocate_window_to_slot(slot_num=closest_slot_num, window=window)
+            self.deallocate_window_from_slot(orig_slot_num)
+            self.allocate_window_to_slot(slot_num=orig_slot_num, window=deallocated_window)
+        elif closest_slot.window is None:
+            # assign the window to the closest slot
+            self.deallocate_window_from_slot(orig_slot_num)
+            self.allocate_window_to_slot(slot_num=closest_slot_num, window=window)
+
+    def assign_window_to_closest_empty_slot(self, window: gw.Window):
+        closest_slot_num = self.get_closest_slot_to_window(window=window, assign_to_empty_slot=True)
+        self.allocate_window_to_slot(slot_num=closest_slot_num, window=window)
 
     def get_center_for_each_slot(self) -> dict[str, tuple[int, int]]:
         """
@@ -89,9 +147,39 @@ class SlotManager:
             dict: A dictionary mapping each slot number to its center coordinates.
         """
         center_coordinates = {}
-        for slot_num, value in self._slots.items():
-            center_coordinates[slot_num] = (value.top / 2, value.left / 2)
+        for slot_num, slot in self._slots.items():
+            center_coordinates[slot_num] = slot.slot_center_coordinates
         return center_coordinates
+
+    def get_slot_from_window(self, window: gw.Window) -> Slot | None:
+        for slot in self.slots.values():
+            if slot.window == window:
+                return slot
+        return None
+
+    def get_slot_num_from_window(self, window: gw.Window) -> str | None:
+        for slot_num, slot in self.slots.items():
+            if slot.window == window:
+                return slot_num
+        return None
+
+    def calculate_distance(
+        self, win_coord: tuple[int, int], slot_coord: tuple[int, int]
+    ) -> int:
+        """
+        calculate the distance between two points in a two-dimensional space (aka desktop coords lol).
+
+        Args:
+            win_coord (tuple[int, int]): Center coordinates of the window as (center_y, center_x).
+            slot_coord (tuple[int, int]): Center coordinates of the slot as (center_y, center_x).
+
+        Returns:
+            int: The distance between the center of the window and the center of the slot coordinates.
+        """
+        return math.sqrt(
+            (win_coord[0] - slot_coord[0]) ** 2
+            + (win_coord[1] - slot_coord[1]) ** 2
+        )
 
     @property
     def allocated_windows(self) -> list[gw.Window]:
@@ -211,23 +299,21 @@ class TableManager:
 
         return bool(deleted_windows), deleted_windows[0] if deleted_windows else (False, None)
 
-    def calculate_distance(
-        self, win_coord: tuple[int, int], slot_coord: tuple[int, int]
-    ) -> int:
+    def is_window_moved(self) -> tuple[bool, gw.Window | None]:
         """
-        calculate the distance between two points in a two-dimensional space (aka desktop coords lol).
-
-        Args:
-            win_coord (tuple[int, int]): Center coordinates of the window as (center_y, center_x).
-            slot_coord (tuple[int, int]): Center coordinates of the slot as (center_y, center_x).
+        Check if any window has been moved from its original position.
 
         Returns:
-            int: The distance between the center of the window and the center of the slot coordinates.
+            tuple: A tuple containing two elements:
+                - A boolean value indicating whether any window has been moved.
+                - If a window has been moved, it returns the moved window; otherwise, it returns None.
         """
-        return math.sqrt(
-            (win_coord[0] - slot_coord[0]) ** 2
-            + (win_coord[1] - slot_coord[1]) ** 2
-        )
+        for slot in self.slot_manager.slots.values():
+            if slot.window:
+                current_position = slot.window.left, slot.window.top
+                if current_position != (slot.left, slot.top):
+                    return True, slot.window
+        return False, None
 
     def arrange_layout_on_start(self):
         def assign_windows_to_slots() -> dict[str, Slot]:
@@ -253,7 +339,7 @@ class TableManager:
                     win_coord_x,
                     target_window,
                 ) in windows_center_coordinates:
-                    distance = self.calculate_distance(
+                    distance = self.slot_manager.calculate_distance(
                         win_coord=(win_coord_y, win_coord_x), slot_coord=slot_coord
                     )
                     distances[distance] = target_window
@@ -279,24 +365,33 @@ class TableManager:
         self.initialize_slots()
         assign_windows_to_slots()
 
+    def get_event(self) -> tuple[EventType | None, gw.Window | None]:
+        has_new_window, new_window = self.is_new_window_detected()
+        is_window_terminated, terminated_window = self.is_window_terminated()
+        is_window_moved, moved_window = self.is_window_moved()
+
+        if has_new_window:
+            return EventType.NEW_WINDOW, new_window
+        elif is_window_terminated:
+            return EventType.WINDOW_TERMINATED, terminated_window
+        elif is_window_moved:
+            return EventType.WINDOW_MOVED, moved_window
+        return None, None
+
     def handle_event(self, event_type: EventType, window: gw.Window):
-            match event_type:
-                case EventType.NEW_WINDOW:
-                    self.handle_new_window_event(window)
-                case EventType.ACTIVE_TAB_CHANGED:
-                    self.handle_active_tab_changed_event(window)
-                case EventType.WINDOW_DELETED:
-                    self.handle_window_terminated_event(window)
+        match event_type:
+            case EventType.NEW_WINDOW:
+                self.handle_new_window_event(window)
+            case EventType.WINDOW_TERMINATED:
+                self.handle_window_terminated_event(window)
+            case EventType.WINDOW_MOVED:
+                self.handle_window_moved_event(window)
 
     def handle_new_window_event(self, window: gw.Window):
-        self.slot_manager.add_window_to_empty_slot(window=window)
-
-    def handle_active_tab_changed_event(self, window: gw.Window):
-        pass
+        self.slot_manager.assign_window_to_closest_empty_slot(window)
 
     def handle_window_terminated_event(self, window: gw.Window):
         """
-        removes it from the tracked windows list and
         deallocates the window if its assigned to a slot
         """
         self.slot_manager.remove_window_from_slot(window)
@@ -305,30 +400,10 @@ class TableManager:
         if unallocated_window:
             self.slot_manager.add_window_to_empty_slot(unallocated_window)
 
-
-
-# def track_processes(app_name: str, table_positions: dict, table_layout: dict):
-#     original_positions = {}  # Dictionary to store the original positions of windows
-
-#     while True:
-#         windows = get_windows_by_title(app_name)
-#         for window in windows:
-#             pid = window._hWnd  # Get the process ID (window handle) as PID
-
-#             # Check if the window's position has changed
-#             if pid not in original_positions:
-#                 # Store the original position for the window
-#                 original_positions[pid] = (window.left, window.top)
-#             else:
-#                 # Compare the current position with the original position
-#                 original_left, original_top = original_positions[pid]
-#                 current_left, current_top = window.left, window.top
-
-#                 if original_left != current_left or original_top != current_top:
-#                     print(f"Window with PID {pid} has moved.")
-
-#             # Get the active tab title for the Chrome/Firefox instance
-#             active_tab_title = get_active_tab_title(app_name, window)
-#             print(f"Window title: {window.title}, Active tab title: {active_tab_title}")
-
-#         time.sleep(1)
+    def handle_window_moved_event(self, window: gw.Window):
+        slot = self.slot_manager.get_slot_from_window(window=window)
+        if not key_states.left_button_pressed: # check if left mouse button is released
+            if slot.is_center_outside_slot_boundary:
+                self.slot_manager.assign_window_to_closest_slot(window=window)
+            else:
+                slot.move_to_assigned_slot()
