a71e2a2dc91e11ce0e6684ee8d9ceb1d9d163ef7
Fruitellar <109595648+Fruitellar@users.noreply.github.com>
2023-09-09 08:55:28 +0800

    added module for managing keyboard and mouse events

---
+++ src/services/input_controllers/entities.py
@@ -0,0 +1,15 @@
+
+class KeyStates:
+    _instance = None
+
+    def __new__(cls, *args, **kwargs):
+        if not cls._instance:
+            cls._instance = super(KeyStates, cls).__new__(cls)
+        return cls._instance
+
+    def __init__(self):
+        self.left_button_pressed = False
+
+
+
+key_states = KeyStates()
---
+++ src/services/input_controllers/mouse_listener.py
@@ -0,0 +1,7 @@
+from pynput import mouse
+from .entities import key_states
+
+def on_click(x, y, button, pressed):
+    if button == mouse.Button.left:
+        key_states.left_button_pressed = pressed
+mouse_listener = mouse.Listener(on_click=on_click)
