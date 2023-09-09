984632ed9c9b8434dc598b1810bcee9c2504578e
Fruitellar <109595648+Fruitellar@users.noreply.github.com>
2023-09-09 09:00:39 +0800

    updated events

--- src/services/tables/events.py
+++ src/services/tables/events.py
@@ -3,5 +3,5 @@ from enum import Enum
 
 class EventType(Enum):
     NEW_WINDOW = 'new_window'
-    ACTIVE_TAB_CHANGED = 'active_tab_changed'
-    WINDOW_DELETED = 'window_deleted'
+    WINDOW_TERMINATED = 'window_terminated'
+    WINDOW_MOVED = 'window_moved'
