6453b55315f4056797916bc9acf1207f6944b8ff
Fruitellar <109595648+Fruitellar@users.noreply.github.com>
2023-09-12 11:32:24 +0800

    mouse controller

---
+++ src/services/input_controllers/mouse_controller.py
@@ -0,0 +1,17 @@
+import pyautogui as pa
+
+
+class MouseController:
+    _instance = None
+
+    def __new__(cls, *args, **kwargs):
+        if not cls._instance:
+            cls._instance = super(MouseController, cls).__new__(cls)
+        return cls._instance
+
+    def get_mouse_coordinates(self):
+        return pa.position()
+
+
+
+mouse_controller = MouseController()
