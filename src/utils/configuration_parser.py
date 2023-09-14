d8ba7637978867aef4f4f9903d842d040a03b58b
Fruitellar <109595648+Fruitellar@users.noreply.github.com>
2023-09-14 10:24:10 +0800

    added hotkeys

--- src/utils/configuration_parser.py
+++ src/utils/configuration_parser.py
@@ -68,54 +68,22 @@ class LayoutConfigurationParser(IConfigurationParser):
             file.write(json_string)
 
 
+class HotkeyConfigurationParser(IConfigurationParser):
+    @staticmethod
+    def read_configuration(filename="hotkey_settings.json", *args, **kwargs):
+        with open(BASE_DIR / "settings" / filename) as settings_file:
+            file_contents = settings_file.read()
+            parsed_settings = json.loads(file_contents)
+            return parsed_settings.get("hotkey_configuration", {})
 
+    @staticmethod
+    def write_configuration(filename="hotkey_settings.json", *args, **kwargs):
+        with open(BASE_DIR / "settings" / filename) as file:
+            file_contents = file.read()
+            parsed_settings = json.loads(file_contents)
 
-# class ConfigurationParser(IConfigurationParser):
-#     @staticmethod
-#     def read_table_configuration(*, filename="table_settings.json"):
-#         with open(BASE_DIR / "settings" / filename) as settings:
-#             file_contents = settings.read()
-#             parsed_settings = json.loads(file_contents)
-#             return parsed_settings["table_configuration"]
+        parsed_settings["hotkey_configuration"] = kwargs['hotkeys']
 
-#     @staticmethod
-#     def write_table_configuration(*, filename="table_settings.json", **kwargs):
-#         with open(BASE_DIR / "settings" / filename) as file:
-#             file_contents = file.read()
-#             parsed_settings = json.loads(file_contents)
-
-#         parsed_settings["table_configuration"] = kwargs
-
-#         with open(BASE_DIR / "settings" / filename, "w") as file:
-#             json_string = json.dumps(parsed_settings, indent=4)
-#             file.write(json_string)
-
-#     @staticmethod
-#     def read_layout_configuration(*, filename="layout_settings.json"):
-#         with open(BASE_DIR / "settings" / filename) as settings_file:
-#             file_contents = settings_file.read()
-#             parsed_settings = json.loads(file_contents)
-#             return parsed_settings.get("layout_configuration", {})
-
-#     @staticmethod
-#     def write_layout_configuration(*, filename="layout_settings.json", **kwargs):
-#         with open(BASE_DIR / "settings" / filename) as file:
-#             file_contents = file.read()
-#             parsed_settings = json.loads(file_contents)
-
-#         layout_configuration = parsed_settings.get("layout_configuration", {})
-
-#         # Update the "table_count" and "table_configurations" in layout_configuration
-#         layout_configuration["table_count"] = kwargs.get(
-#             "table_count", layout_configuration["table_count"]
-#         )
-#         layout_configuration["table_configurations"] = kwargs.get(
-#             "table_configurations", layout_configuration["table_configurations"]
-#         )
-
-#         # Update the "layout_configuration" in the main parsed_settings
-#         parsed_settings["layout_configuration"] = layout_configuration
-
-#         with open(BASE_DIR / "settings" / filename, "w") as file:
-#             json_string = json.dumps(parsed_settings, indent=4)
-#             file.write(json_string)
+        with open(BASE_DIR / "settings" / filename, "w") as file:
+            json_string = json.dumps(parsed_settings, indent=4)
+            file.write(json_string)
